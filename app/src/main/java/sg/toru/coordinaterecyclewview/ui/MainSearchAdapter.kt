4e0364a119ea9019441af3e0b12b57298e82c3ef
Toru Choi <blueocean239@gmail.com>
2020-03-10 18:19:00 +0800

    add infinite scroll adapter

--- app/src/main/java/sg/toru/coordinaterecyclewview/ui/MainSearchAdapter.kt
+++ app/src/main/java/sg/toru/coordinaterecyclewview/ui/MainSearchAdapter.kt
@@ -1,32 +1,42 @@
 package sg.toru.coordinaterecyclewview.ui
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import sg.toru.coordinaterecyclewview.databinding.ItemSearchBinding
+import java.util.*
 
-class MainSearchAdapter(private val itemCallback: MainItemCallback): ListAdapter<String, SearchViewHolder>(itemCallback) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+class MainSearchAdapter(private val itemCallback: MainItemCallback): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    var recyclerviewListItem:LinkedList<String> = LinkedList()
+    set(value) {
+        val currentSize = field.size
+        field = value
+        notifyItemInserted(currentSize)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val binding = ItemSearchBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return SearchViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
-        holder.bindItem(getItem(position))
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as SearchViewHolder).bindItem(recyclerviewListItem[position])
     }
+
+    override fun getItemCount(): Int = recyclerviewListItem.size
 }
 
 class MainItemCallback(): DiffUtil.ItemCallback<String>(){
     override fun areItemsTheSame(oldItem: String, newItem: String): Boolean = (oldItem == newItem)
-
     override fun areContentsTheSame(oldItem: String, newItem: String): Boolean = (oldItem == newItem)
-
 }
 
 class SearchViewHolder(private val binding: ItemSearchBinding):RecyclerView.ViewHolder(binding.root) {
-    fun bindItem(str:String) {
-
-    }
+    fun bindItem(str:String) {}
 }
+
+class LoadingViewHolder(view: View):RecyclerView.ViewHolder(view)
--- app/src/main/java/sg/toru/coordinaterecyclewview/ui/MainSearchFragment.kt
+++ app/src/main/java/sg/toru/coordinaterecyclewview/ui/MainSearchFragment.kt
@@ -1,13 +1,17 @@
 package sg.toru.coordinaterecyclewview.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 
 import sg.toru.coordinaterecyclewview.R
 import sg.toru.coordinaterecyclewview.databinding.FragmentMainSearchBinding
+import java.util.*
 
 /**
  * A simple [Fragment] subclass.
@@ -15,6 +19,39 @@ import sg.toru.coordinaterecyclewview.databinding.FragmentMainSearchBinding
 class MainSearchFragment : Fragment() {
 
     private lateinit var binding:FragmentMainSearchBinding
+
+    private val loadMoreListener:OnLoadMoreListener by lazy {
+        object:OnLoadMoreListener{
+            override fun onLoadMore() {
+                Log.e("Toru", "onLoadMore!!")
+
+                Handler().postDelayed({
+                    with((binding.RcvSearch.adapter as MainSearchAdapter)){
+                        val currentSizeBefore = itemCount
+                        val arrayList = LinkedList<String>()
+                        arrayList.add("11")
+                        arrayList.add("12")
+                        arrayList.add("13")
+                        arrayList.add("14")
+                        arrayList.add("15")
+                        arrayList.add("16")
+                        arrayList.add("17")
+                        arrayList.add("18")
+                        arrayList.add("19")
+                        arrayList.add("20")
+                        recyclerviewListItem.addAll(recyclerviewListItem.size, arrayList)
+                        notifyItemInserted(currentSizeBefore)
+                    }
+
+                    scrollListener.setLoaded()
+                }, 300)
+            }
+        }
+    }
+
+    private val scrollListener:InfiniteScrollListener by lazy {
+        InfiniteScrollListener((binding.RcvSearch.layoutManager as GridLayoutManager), loadMoreListener)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,8 +67,24 @@ class MainSearchFragment : Fragment() {
         view: View,
         savedInstanceState: Bundle?
     ) {
+
+        binding.RcvSearch.addOnScrollListener(scrollListener)
         binding.RcvSearch.adapter = MainSearchAdapter(MainItemCallback())
-        (binding.RcvSearch.adapter as MainSearchAdapter).submitList(listOf("1","2","1","2","1","2","1","2","1","2"))
+
+        val list = LinkedList<String>().apply {
+            add("1")
+            add("2")
+            add("3")
+            add("4")
+            add("5")
+            add("6")
+            add("7")
+            add("8")
+            add("9")
+            add("10")
+        }
+
+        (binding.RcvSearch.adapter as MainSearchAdapter).recyclerviewListItem = list
     }
 
     companion object {
